20cb4a6cad8cb4a81f9688d011e2a7e5611bf2ed
Northwalker <northwalker28@gmail.com>
2023-11-08 13:59:14 +0800

    Update solutions

---
+++ src/algorithms/2849-determine-if-a-cell-is-reachable-at-a-given-time.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} sx
+ * @param {number} sy
+ * @param {number} fx
+ * @param {number} fy
+ * @param {number} t
+ * @return {boolean}
+ */
+// Approach: Math
+const isReachableAtTime = function (sx, sy, fx, fy, t) {
+  const width = Math.abs(sx - fx);
+  const height = Math.abs(sy - fy);
+  if (width === 0 && height === 0 && t === 1) {
+    return false;
+  }
+  return t >= Math.max(width, height);
+};
